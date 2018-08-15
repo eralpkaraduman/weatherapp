ddd83a5b51acc8dff1454ef5b26abd52671e46d6
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-08-15 23:05:16 +0300

    Adds ability to validate&set verified CORS header

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,22 +7,25 @@ const cors = require('kcors');
 
 const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
-const corsOrigin = process.env.CORS_ORIGIN;
-const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
-debug('corsOrigin: ' + corsOrigin);
+// expects CORS_ALLOWED_ORIGINS to be in , seperated format ie: "http://a.com,https://b.com,http://c.com"
+const corsAllowedOrigins = (process.env.CORS_ALLOWED_ORIGINS || '').split(',');
+console.log(`corsAllowedOrigins: "${corsAllowedOrigins}"`);
+
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
 const app = new Koa();
 
-let corsOptions = {};
-if (corsOrigin) {
-  console.log(`cors.origin = ${corsOrigin}`);
-  corsOptions.origin = corsOrigin;
+function verifyOrigin (ctx) {
+  var origin = ctx.headers.origin;
+  if (corsAllowedOrigins.length > 0 && corsAllowedOrigins.indexOf(origin) !== -1) {
+    ctx.set('Access-Control-Allow-Origin', origin);
+  }
 }
 
-app.use(cors(corsOptions));
+app.use(cors(verifyOrigin));
 
 const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
